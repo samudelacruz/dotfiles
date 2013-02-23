864ffeaadb5949c731be1281d3170b12271d295e
Samuel de la Cruz <samudelacruz@gmail.com>
2013-02-23 11:25:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+git clone git://github.com/samudelacruz/dotfiles.git
+dotfiles/install.sh
+
+Extra thanks to Mitchell Hashimoto and Paul Irish for the ideas
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#get z from rupa´s repo and install at ~/code/z
+mkdir -p $HOME/code/z
+cd $HOME/code/z
+git clone https://github.com/rupa/z
+
+#copy files to home directory
+
+cp bashrc $HOME/.bashrc
+cp aliases $HOME/.aliases
+cp functions $HOME/.functions
+cp ackrc $HOME/.ackrc
+cp bash_prompt $HOME/.bash_prompt
+cp osx $HOME/.osx #execute with . .osx if you want the defaults check before
+cp inputrc $HOME/.inputrc
+
